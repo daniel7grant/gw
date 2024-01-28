19dc6ac1bd047130ee50cdea3c536d189169838d
Grant <gergely.daniel7@gmail.com>
2024-01-28 19:42:50 +0100

    Add once trigger

--- src/triggers/once.rs
+++ src/triggers/once.rs
@@ -2,16 +2,31 @@ use super::Trigger;
 use crate::Result;
 use std::sync::mpsc::Sender;
 
-/// A trigger that runs the checks once and then exits
+/// A trigger that runs the checks once and then exits.
 pub struct OnceTrigger;
 
 impl Trigger for OnceTrigger {
+    /// Starts a trigger that runs once and terminates after.
     fn listen(&self, tx: Sender<Option<()>>) -> Result<()> {
         tx.send(Some(()))?;
-		tx.send(None)?;
+        tx.send(None)?;
         Ok(())
     }
 }
 
 #[cfg(test)]
-mod tests {}
+mod tests {
+    use super::*;
+    use std::sync::mpsc;
+
+    #[test]
+    fn it_should_trigger_once_and_stop() {
+        let trigger = OnceTrigger;
+        let (tx, rx) = mpsc::channel::<Option<()>>();
+
+        trigger.listen(tx).unwrap();
+
+        let msgs: Vec<_> = rx.iter().collect();
+        assert_eq!(vec![Some(()), None], msgs);
+    }
+}
