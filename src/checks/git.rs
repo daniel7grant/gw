5a3a5d0614deb215e67d848d447ea01576d815dd
Grant <gergely.daniel7@gmail.com>
2024-02-02 21:51:22 +0100

    Add custom error handling to git repository

--- src/checks/git.rs
+++ src/checks/git.rs
@@ -1,17 +1,19 @@
 use self::repository::GitRepository;
-
 use super::Check;
-use crate::Result;
+use crate::Result as GwResult;
 
-mod repository;
 mod credentials;
+mod repository;
 
 /// A check to fetch and pull a local git repository.
+///
+/// This will update the repository, if there are any changes.
+/// In case there are any local changes, these might be erased.
 pub struct GitCheck(pub GitRepository);
 
 impl GitCheck {
     /// Open the git repository at the given directory.
-    pub fn open(directory: &str) -> Result<Self> {
+    pub fn open(directory: &str) -> GwResult<Self> {
         let repo = GitRepository::open(directory)?;
         Ok(GitCheck(repo))
     }
@@ -20,7 +22,7 @@ impl GitCheck {
 impl Check for GitCheck {
     /// Fetch and pull changes from the remote repository on the current branch.
     /// It returns true if the pull was successful and there are new changes.
-    fn check(&mut self) -> Result<bool> {
+    fn check(&mut self) -> GwResult<bool> {
         let GitCheck(repo) = self;
         let fetch_commit = repo.fetch()?;
         if repo.check_if_updatable(&fetch_commit)? && repo.pull(&fetch_commit)? {
@@ -33,8 +35,8 @@ impl Check for GitCheck {
 
 #[cfg(test)]
 mod tests {
+    use super::repository::GitError;
     use super::*;
-    use crate::Result;
     use duct::cmd;
     use rand::distributions::{Alphanumeric, DistString};
     use std::{fs, path::Path};
@@ -43,7 +45,7 @@ mod tests {
         Alphanumeric.sample_string(&mut rand::thread_rng(), 16)
     }
 
-    fn create_empty_repository(local: &str) -> Result<()> {
+    fn create_empty_repository(local: &str) -> GwResult<()> {
         let remote = format!("{local}-remote");
 
         // Create directory and repository in it
@@ -58,7 +60,7 @@ mod tests {
         Ok(())
     }
 
-    fn create_other_repository(local: &str) -> Result<()> {
+    fn create_other_repository(local: &str) -> GwResult<()> {
         let remote = format!("{local}-remote");
         let other = format!("{local}-other");
 
@@ -72,12 +74,14 @@ mod tests {
         Ok(())
     }
 
-    fn cleanup_repository(local: &str) -> Result<()> {
+    fn cleanup_repository(local: &str) -> GwResult<()> {
         let remote = format!("{local}-remote");
         let other = format!("{local}-other");
 
         fs::remove_dir_all(local)?;
-        fs::remove_dir_all(remote)?;
+        if Path::new(&remote).exists() {
+            fs::remove_dir_all(remote)?;
+        }
         if Path::new(&other).exists() {
             fs::remove_dir_all(other)?;
         }
@@ -85,8 +89,35 @@ mod tests {
         Ok(())
     }
 
+    fn create_failing_repository(local: &str, create_commit: bool) -> GwResult<()> {
+        fs::create_dir(&local)?;
+        cmd!("git", "init").dir(&local).read()?;
+
+        if create_commit {
+            fs::write(&format!("{local}/1"), "1")?;
+            cmd!("git", "add", "-A").dir(&local).read()?;
+            cmd!("git", "commit", "-m1").dir(&local).read()?;
+        }
+
+        Ok(())
+    }
+
+    fn create_merge_conflict(local: &str) -> GwResult<()> {
+        let other = format!("{local}-other");
+
+        fs::write(&format!("{local}/1"), "11")?;
+        cmd!("git", "add", "-A").dir(&local).read()?;
+        cmd!("git", "commit", "-m1").dir(&local).read()?;
+
+        fs::write(&format!("{other}/1"), "12")?;
+        cmd!("git", "add", "-A").dir(&other).read()?;
+        cmd!("git", "commit", "-m2").dir(&other).read()?;
+
+        Ok(())
+    }
+
     #[test]
-    fn it_should_open_a_repository() -> Result<()> {
+    fn it_should_open_a_repository() -> GwResult<()> {
         let id = get_random_id();
         let local = format!("test_directories/{id}");
 
@@ -100,7 +131,64 @@ mod tests {
     }
 
     #[test]
-    fn it_should_return_false_if_the_remote_didnt_change() -> Result<()> {
+    fn it_should_fail_if_path_is_invalid() -> GwResult<()> {
+        let result = GitCheck::open("/path/to/nowhere").err().unwrap();
+        let error = *result.downcast::<GitError>()?;
+
+        assert!(
+            matches!(error, GitError::NotAGitRepository(_)),
+            "{error:?} should be NotAGitRepository"
+        );
+
+        Ok(())
+    }
+
+    #[test]
+    fn it_should_fail_if_we_are_not_on_a_branch() -> GwResult<()> {
+        let id = get_random_id();
+        let local = format!("test_directories/{id}");
+
+        // Don't create commit to create an empty repository
+        create_failing_repository(&local, false)?;
+
+        let mut check: GitCheck = GitCheck::open(&local)?;
+        let result = check.check().err().unwrap();
+        let error = *result.downcast::<GitError>()?;
+
+        assert!(
+            matches!(error, GitError::NotOnABranch),
+            "{error:?} should be NotOnABranch"
+        );
+
+        cleanup_repository(&local)?;
+
+        Ok(())
+    }
+
+    #[test]
+    fn it_should_fail_if_there_is_no_remote() -> GwResult<()> {
+        let id = get_random_id();
+        let local = format!("test_directories/{id}");
+
+        // Don't create commit to create an empty repository
+        create_failing_repository(&local, true)?;
+
+        let mut check: GitCheck = GitCheck::open(&local)?;
+        let result = check.check().err().unwrap();
+        let error = *result.downcast::<GitError>()?;
+
+        assert!(
+            matches!(error, GitError::NoRemoteForBranch(_)),
+            "{error:?} should be NoRemoteForBranch"
+        );
+
+        cleanup_repository(&local)?;
+
+        Ok(())
+    }
+
+    #[test]
+    fn it_should_return_false_if_the_remote_didnt_change() -> GwResult<()> {
         let id = get_random_id();
         let local = format!("test_directories/{id}");
 
@@ -116,13 +204,13 @@ mod tests {
     }
 
     #[test]
-    fn it_should_return_true_if_the_remote_changes() -> Result<()> {
+    fn it_should_return_true_if_the_remote_changes() -> GwResult<()> {
         let id = get_random_id();
         let local = format!("test_directories/{id}");
 
         create_empty_repository(&local)?;
 
-        // Create another repository and push a new commit 
+        // Create another repository and push a new commit
         create_other_repository(&local)?;
 
         let mut check = GitCheck::open(&local)?;
@@ -131,6 +219,67 @@ mod tests {
 
         // The pushed file should be pulled
         assert!(Path::new(&format!("{local}/2")).exists());
+
+        cleanup_repository(&local)?;
+
+        Ok(())
+    }
+
+    #[test]
+    fn it_should_fail_if_there_is_a_merge_conflict() -> GwResult<()> {
+        let id = get_random_id();
+        let local = format!("test_directories/{id}");
+
+        create_empty_repository(&local)?;
+
+        // Create another repository and push a new commit
+        create_other_repository(&local)?;
+
+        // Modify the same file in both directories to create a merge conflict
+        create_merge_conflict(&local)?;
+
+        let mut check = GitCheck::open(&local)?;
+        let result = check.check().err().unwrap();
+        let error = *result.downcast::<GitError>()?;
+
+        assert!(
+            matches!(error, GitError::MergeConflict),
+            "{error:?} should be MergeConflict"
+        );
+
+        cleanup_repository(&local)?;
+
+        Ok(())
+    }
+
+    #[test]
+    fn it_should_fail_if_repository_is_not_accessible() -> GwResult<()> {
+        let id = get_random_id();
+        let local = format!("test_directories/{id}");
+
+        create_empty_repository(&local)?;
+
+        // Create another repository and push a new commit
+        create_other_repository(&local)?;
+
+        // Set repository to readonly
+        let mut perms = fs::metadata(&local)?.permissions();
+        perms.set_readonly(true);
+        fs::set_permissions(&local, perms)?;
+
+        let mut check: GitCheck = GitCheck::open(&local)?;
+        let result = check.check().err().unwrap();
+        let error = *result.downcast::<GitError>()?;
+
+        assert!(
+            matches!(error, GitError::FailedSettingHead(_)),
+            "{error:?} should be FailedSettingHead"
+        );
+
+        // Set repository to readonly
+        let mut perms = fs::metadata(&local)?.permissions();
+        perms.set_readonly(false);
+        fs::set_permissions(&local, perms)?;
 
         cleanup_repository(&local)?;
 
--- src/checks/git/repository.rs
+++ src/checks/git/repository.rs
@@ -1,36 +1,66 @@
+use std::fmt::Debug;
+
 use super::credentials::CredentialHandler;
-use crate::Result;
-use git2::{
-    AnnotatedCommit, AutotagOption, Error as GitError, FetchOptions, RemoteCallbacks, Repository,
-};
+use git2::{AnnotatedCommit, AutotagOption, FetchOptions, RemoteCallbacks, Repository};
+use thiserror::Error;
 
 pub struct GitRepository {
     repo: Repository,
 }
 
+/// Custom error describing the error cases for the GitCheck.
+#[derive(Debug, Error)]
+pub enum GitError {
+    /// The directory is not a valid git repository.
+    #[error("{0} does not exist, user don't have access or not a git repository")]
+    NotAGitRepository(String),
+    /// There is no branch in the repository currently. It can be a repository
+    /// without any branch, or checked out on a commit.
+    #[error("we are currently not on a branch")]
+    NotOnABranch,
+    /// There is no remote for the current branch. This usually because the branch hasn't been pulled.
+    #[error("branch {0} doesn't have a remote")]
+    NoRemoteForBranch(String),
+    /// There are changes in the directory, avoiding pulling. This is a safety mechanism to avoid pulling
+    /// over local changes, to not overwrite anything important.
+    #[error("there are changes in the directory, not pulling")]
+    DirtyWorkingTree,
+    /// Cannot fetch the current branch. This is possibly a network failure.
+    #[error("cannot fetch, might be a network error")]
+    FetchFailed,
+    /// Cannot pull updates to the current branch. This means either the merge analysis failed
+    /// or there is a merge conflict.
+    #[error("cannot update branch, there is likely a merge conflict")]
+    MergeConflict,
+    /// Cannot set the HEAD to the fetch commit.
+    #[error("could not set HEAD to fetch commit {0}, might be permission issues")]
+    FailedSettingHead(String),
+}
+
 impl GitRepository {
-    pub fn open(directory: &str) -> Result<Self> {
-        let repo = Repository::open(directory).map_err(|err| err.to_string())?;
+    pub fn open(directory: &str) -> Result<Self, GitError> {
+        let repo = Repository::open(directory)
+            .map_err(|_| GitError::NotAGitRepository(String::from(directory)))?;
 
         Ok(GitRepository { repo })
     }
 
     // Inspired from: https://github.com/rust-lang/git2-rs/blob/master/examples/pull.rs
-    pub fn fetch(&self) -> Result<AnnotatedCommit> {
+    pub fn fetch(&self) -> Result<AnnotatedCommit, GitError> {
         let Self { repo, .. } = self;
-        let head = repo.head()?;
-        let branch_name = head
-            .shorthand()
-            .ok_or(GitError::from_str("We are currently not on a branch."))?;
-        let branch_name_ref = head
-            .name()
-            .ok_or(GitError::from_str("We are currently not on a branch."))?;
-        let remote_buf = repo.branch_upstream_remote(branch_name_ref)?;
+        let head = repo.head().map_err(|_| GitError::NotOnABranch)?;
+        let branch_name = head.shorthand().ok_or(GitError::NotOnABranch)?;
+        let branch_name_ref = head.name().ok_or(GitError::NotOnABranch)?;
+        let remote_buf = repo
+            .branch_upstream_remote(branch_name_ref)
+            .map_err(|_| GitError::NoRemoteForBranch(String::from(branch_name)))?;
         let remote_name = remote_buf
             .as_str()
-            .ok_or(GitError::from_str("This branch doesn't have a remote."))?;
+            .ok_or_else(|| GitError::NoRemoteForBranch(String::from(branch_name)))?;
 
-        let mut remote = repo.find_remote(remote_name)?;
+        let mut remote = repo
+            .find_remote(remote_name)
+            .map_err(|_| GitError::NoRemoteForBranch(String::from(branch_name)))?;
 
         let mut cb = RemoteCallbacks::new();
         let git_config = git2::Config::open_default().unwrap();
@@ -43,54 +73,67 @@ impl GitRepository {
         opts.remote_callbacks(cb);
         opts.download_tags(AutotagOption::All);
 
-        remote.fetch(&[branch_name], Some(&mut opts), None)?;
+        remote
+            .fetch(&[branch_name], Some(&mut opts), None)
+            .map_err(|_| GitError::FetchFailed)?;
 
-        let fetch_head = repo.find_reference("FETCH_HEAD")?;
-        let fetch_commit = repo.reference_to_annotated_commit(&fetch_head)?;
+        let fetch_head = repo
+            .find_reference("FETCH_HEAD")
+            .map_err(|_| GitError::FetchFailed)?;
+        let fetch_commit = repo
+            .reference_to_annotated_commit(&fetch_head)
+            .map_err(|_| GitError::FetchFailed)?;
 
         Ok(fetch_commit)
     }
 
-    pub fn check_if_updatable(&self, fetch_commit: &AnnotatedCommit) -> Result<bool> {
+    pub fn check_if_updatable(&self, fetch_commit: &AnnotatedCommit) -> Result<bool, GitError> {
         let Self { repo, .. } = self;
-        let (analysis, _) = repo.merge_analysis(&[fetch_commit])?;
+        let (analysis, _) = repo
+            .merge_analysis(&[fetch_commit])
+            .map_err(|_| GitError::MergeConflict)?;
 
         if analysis.is_fast_forward() {
             Ok(true)
         } else if analysis.is_up_to_date() {
             Ok(false)
         } else {
-            Err(Box::<dyn std::error::Error>::from(GitError::from_str(
-                "Could not update branch. Possibly there is a merge conflict.",
-            )))
+            Err(GitError::MergeConflict)
         }
     }
 
-    pub fn pull(&self, fetch_commit: &AnnotatedCommit) -> Result<bool> {
+    pub fn pull(&self, fetch_commit: &AnnotatedCommit) -> Result<bool, GitError> {
         let Self { repo, .. } = self;
-        let head = repo.head()?;
-        let branch_name = head
-            .shorthand()
-            .ok_or(GitError::from_str("We are currently not on a branch."))?;
+        let head = repo.head().map_err(|_| GitError::NotOnABranch)?;
+        let branch_name = head.shorthand().ok_or_else(|| GitError::NotOnABranch)?;
 
-        // TODO: Only fetch if the repository is dirty
+        // TODO: Only fetch if the repository is not dirty
 
         let branch_refname = format!("refs/heads/{}", branch_name);
-        let branch_ref = repo.find_reference(&branch_refname)?;
+        let branch_ref = repo
+            .find_reference(&branch_refname)
+            .map_err(|_| GitError::NotOnABranch)?;
 
         let name = branch_ref
             .name()
-            .ok_or(GitError::from_str("Remote branch name is invalid."))?;
+            .ok_or_else(|| GitError::NoRemoteForBranch(String::from(branch_name)))?;
         let msg = format!(
             "Fast-Forward: Setting {} to id: {}",
             name,
             fetch_commit.id()
         );
 
-        let mut branch_ref = repo.find_reference(&branch_refname)?;
-        branch_ref.set_target(fetch_commit.id(), &msg)?;
-        repo.set_head(name)?;
-        repo.checkout_head(Some(git2::build::CheckoutBuilder::default().force()))?;
+        let mut branch_ref = repo
+            .find_reference(&branch_refname)
+            .map_err(|_| GitError::NotOnABranch)?;
+        let fetch_id = fetch_commit.id();
+        branch_ref
+            .set_target(fetch_id, &msg)
+            .map_err(|_| GitError::FailedSettingHead(fetch_id.to_string()))?;
+        repo.set_head(name)
+            .map_err(|_| GitError::FailedSettingHead(fetch_id.to_string()))?;
+        repo.checkout_head(Some(git2::build::CheckoutBuilder::default().force()))
+            .map_err(|_| GitError::FailedSettingHead(fetch_id.to_string()))?;
         Ok(true)
     }
 }
